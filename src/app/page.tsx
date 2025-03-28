6ece863de94c2c5bac2477f9a7dfe41c666f01fd
Francisco Munoz <franciscomuniozs@gmail.com>
2025-03-28 13:33:37 -0300

    Added components for every section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import { CTASection } from "@/components/ui/cta-section";
+import { Footer } from "@/components/ui/footer";
+import { Hero } from "@/components/ui/hero";
+import { TechStack } from "@/components/ui/tech-stack";
+import { Workflow } from "@/components/ui/workflow";
 
 export default function Home() {
   return (
-    <div className="container max-7w-xl mx-auto px-4">
-      <h1 className="text-2xl font-bold text-center mt-8">Hello World</h1>
-    </div>
+    <>
+      <main>
+        <Hero />
+        <TechStack />
+        <Workflow />
+        <CTASection />
+      </main>
+      <Footer />
+    </>
   );
 }
